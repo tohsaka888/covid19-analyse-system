b827a1aaccb0afe794381fa80c504af6d4294598
tohsaka888 <156132264@qq.com>
2022-05-30 17:00:21 +0800

    feat: 主页

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Container from "tohsaka888-word-cloud";
+import font from "./font.ttf";
+import Header from "./components/Header";
+import { LangContext } from "./Context/LangContext";
+import { useColorMode } from "@chakra-ui/react";
+
+type ResponseData = { results: string[]; success: boolean };
 
 function App() {
+  const [province, setProvince] = useState<string[]>([]);
+  const [lang, setLang] = useState<"zh" | "en">("zh");
+  const { colorMode } = useColorMode();
+  useEffect(() => {
+    const getProvinceName = async () => {
+      const res = await fetch(
+        `https://lab.isaaclin.cn/nCoV/api/provinceName?lang=${lang}`
+      );
+      const data: ResponseData = await res.json();
+      setProvince(data.results);
+    };
+    getProvinceName();
+  }, [lang]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LangContext.Provider value={{ lang, setLang }}>
+      <Header />
+      <Container
+        items={province}
+        type={"text"}
+        radius={20}
+        width="100%"
+        height="100vh"
+        color={colorMode === "light" ? "#666666" : "#fff"}
+        fontSize={1.2}
+        font={font}
+      />
+    </LangContext.Provider>
   );
 }
 
