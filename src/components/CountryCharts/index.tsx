29510ac3fcefc0b4391ecc910ba98c22d098121e
tohsaka888 <156132264@qq.com>
2022-05-31 17:28:17 +0800

    feat: 国家图标

--- src/components/CountryCharts/index.tsx
+++ src/components/CountryCharts/index.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactECharts, { EChartsReactProps } from "echarts-for-react";
+import { option as defaultOptions } from "./options";
+import useScreenSize from "../hooks/useScreenSize/useScreenSize";
+import { AreaInfoResults } from "../../type";
 
-function CountryCharts() {
-  return <div>CountryCharts</div>;
-}
+type Props = {
+  source?: AreaInfoResults;
+};
+
+const CountryCharts = React.memo(function ({
+  option,
+  style,
+  source,
+  ...props
+}: EChartsReactProps & Props) {
+  const size = useScreenSize();
+  const [newOptions, setNewOptions] = useState<EChartsReactProps["option"]>({
+    ...option,
+    ...defaultOptions,
+  });
+
+  useEffect(() => {
+    if (source) {
+      setNewOptions((newOptions: any) => ({
+        ...newOptions,
+        series: [
+          {
+            ...newOptions.series[0],
+            data: [
+              { value: source?.currentConfirmedCount, name: "现存确诊人数" },
+              { value: source?.confirmedCount, name: "累计确诊人数" },
+              { value: source?.suspectedCount, name: "疑似感染人数" },
+              { value: source?.curedCount, name: "治愈人数" },
+              { value: source?.deadCount, name: "死亡人数" },
+            ],
+          },
+        ],
+      }));
+    }
+  }, [
+    source,
+    source?.confirmedCount,
+    source?.curedCount,
+    source?.currentConfirmedCount,
+    source?.deadCount,
+    source?.suspectedCount,
+  ]);
+
+  return (
+    <ReactECharts
+      style={{
+        height: (size.height - 220) / 2,
+        width: "20vw",
+        margin: "0 auto",
+        ...style,
+      }}
+      {...props}
+      option={newOptions}
+    />
+  );
+});
 
 export default CountryCharts;
---
+++ src/components/CountryCharts/options.ts
@@ -0,0 +1,50 @@
+import { EChartsReactProps } from "echarts-for-react";
+
+export const option: EChartsReactProps["option"] = {
+  title: {
+    text: '当前选择区域疫情状况',
+    subtext: '内容来源--丁香网',
+    x: 'center',
+    textStyle: {
+      color: '#fff'
+    }
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: "{a} <br/>{b} : {c} ({d}%)",
+    color: '#fff',
+    textStyle: {
+      color: '#fff'
+    }
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left',
+    data: ['现存确诊人数', '累计确诊人数', '疑似感染人数', '治愈人数', '死亡人数'],
+    textStyle: {
+      color: '#fff'
+    }
+  },
+  series: [
+    {
+      name: '人数',
+      type: 'pie',
+      radius: '55%',
+      center: ['50%', '60%'],
+      data: [
+        { value: 335, name: '现存确诊人数' },
+        { value: 310, name: '累计确诊人数' },
+        { value: 234, name: '疑似感染人数' },
+        { value: 135, name: '治愈人数' },
+        { value: 1548, name: '死亡人数' }
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
