c57674a006b96a6d316685f54b155249f64296bb
tohsaka888 <156132264@qq.com>
2022-05-31 15:35:01 +0800

    feat: 全局shadow支持

--- src/theme.ts
+++ src/theme.ts
@@ -8,8 +8,14 @@ const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: true,
 }
+// '0px 0px 10px 5px #f9f9f9' : '0px 0px 20px 10px #1b2d50'
+const shadows = {
+  light: '0px 0px 10px 5px #e8e8e8',
+  dark: '0px 0px 10px 5px #1b2d50',
+}
 
 // 3. extend the theme
 const theme = extendTheme({ config })
 
 export default theme
+export { shadows }
