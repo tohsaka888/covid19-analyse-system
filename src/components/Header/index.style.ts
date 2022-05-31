885809547e56ebb190c4fec89eab825c98cc656e
tohsaka888 <156132264@qq.com>
2022-05-31 15:37:13 +0800

    style: 更新样式

--- src/components/Header/index.style.ts
+++ src/components/Header/index.style.ts
@@ -5,5 +5,5 @@ export const Container = styled.div<{ color: 'light' | 'dark' }>`
   justify-content: space-between;
   align-items: center;
   height: 60px;
-  box-shadow: ${({ color }) => color === 'light' ? '0px 0px 10px 5px #f9f9f9' : '0px 0px 20px 10px #1b2d50'};
+  box-shadow: ${({ color }) => color === 'light' ? '0px 0px 10px 5px #e8e8e8' : '0px 0px 20px 10px #1b2d50'};
 `
