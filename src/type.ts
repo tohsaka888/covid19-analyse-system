554deb4fffd156d782f375f1afff488c4a31aa6e
tohsaka888 <156132264@qq.com>
2022-05-31 15:35:11 +0800

    feat: 新增类型

--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,28 @@ export type LangContextProps = {
   lang: 'zh' | 'en'
   setLang: React.Dispatch<React.SetStateAction<'zh' | 'en'>>
 }
+
+export type AreaInfoResults = {
+  locationId: number;
+  continentName: string;
+  continentEnglishName: string;
+  countryName: string;
+  countryEnglishName: string;
+  countryFullName: string;
+  provinceName: string;
+  provinceEnglishName: string;
+  provinceShortName: string;
+  currentConfirmedCount: number;
+  confirmedCount: number;
+  suspectedCount: number;
+  curedCount: number;
+  deadCount: number;
+  comment: object;
+  cities?: any[];
+  updateTime: number;
+}
+
+export type AreaInfo = {
+  results: AreaInfoResults[];
+  success: boolean;
+};
