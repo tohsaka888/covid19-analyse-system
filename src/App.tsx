eb0c84062805a44169c735c3645ee497f4e3784c
tohsaka888 <156132264@qq.com>
2022-06-01 17:20:47 +0800

    feat: 默认词云修改为中国地图

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import BriefInfo from "./components/BriefInfo";
 import CountryCharts from "./components/CountryCharts";
 import SearchArea from "./components/SearchArea";
 import MapSelector from "./components/MapSelector";
+import { ModeContext } from "./Context/ModeContext";
+import EchartsMap from "./components/EchartsMap/EchartsMap";
 
 type ResponseData = { results: string[]; success: boolean };
 
@@ -23,8 +25,9 @@ function App() {
   const [province, setProvince] = useState<string[]>([]);
   const [lang, setLang] = useState<"zh" | "en">("zh");
   const { colorMode } = useColorMode();
-  const [area, setArea] = useState<string>(lang === "zh" ? "中国" : "China");
+  const [area, setArea] = useState<string>(lang === "zh" ? "江苏省" : "China");
   const [areaInfo, setAreaInfo] = useState<AreaInfoResults[]>([]);
+  const [mode, setMode] = useState<'词云' | '中国地图'>("中国地图");
   useEffect(() => {
     const getProvinceName = async () => {
       const province =
@@ -80,80 +83,89 @@ function App() {
   const size = useScreenSize();
   return (
     <LangContext.Provider value={{ lang, setLang }}>
-      <Header />
-      <Content style={{ height: size.height - 60 + "px" }}>
-        <Sider
-          position="left"
-          style={{
-            height: "100%",
-            width: "25vw",
-            borderRight: `3px solid ${colorMode === "light" ? "#f9f9f9" : "#1b2d50"
-              }`,
-            padding: "8px 16px",
-          }}
-        >
-          <SearchArea style={{ height: size.height - 440 - 200 }} />
-          <Card
+      <ModeContext.Provider value={{ mode, setMode }}>
+        <Header />
+        <Content style={{ height: size.height - 60 + "px" }}>
+          <Sider
+            position="left"
             style={{
-              width: "100%",
-              height: "max-content",
-              minHeight: "150px",
+              height: "100%",
+              width: "25vw",
+              borderRight: `3px solid ${colorMode === "light" ? "#f9f9f9" : "#1b2d50"
+                }`,
+              padding: "8px 16px",
             }}
-            key={area + 1}
           >
-            <CountryInfo areaInfo={areaInfo} />
-          </Card>
-          <Card delay={300} key={area + 2} style={{ width: "100%" }}>
-            <BriefInfo areaInfo={areaInfo} />
-          </Card>
-        </Sider>
-        <Content>
-          <Box pos={'absolute'} top={'30px'} right={'30px'} zIndex={100} >
-            <MapSelector />
-          </Box>
-          <Container
-            items={province}
-            type={"text"}
-            radius={20}
-            width={"50vw"}
-            height={"100%"}
-            color={colorMode === "light" ? "#666666" : "#fff"}
-            fontSize={1.2}
-            font={font}
-            clickEvent={(e, item) => {
-              console.log(item);
-              setArea(item);
+            <SearchArea style={{ height: size.height - 440 - 200 }} >
+              <EchartsMap data={[]} max={0} style={{ width: size.width / 4 - 30, height: '400px' }} theme={colorMode} info={areaInfo[0]} clickEvent={() => { }} />
+            </SearchArea>
+            <Card
+              style={{
+                width: "100%",
+                height: "max-content",
+                minHeight: "150px",
+              }}
+              key={area + 1}
+            >
+              <CountryInfo areaInfo={areaInfo} />
+            </Card>
+            <Card delay={300} key={area + 2} style={{ width: "100%" }}>
+              <BriefInfo areaInfo={areaInfo} />
+            </Card>
+          </Sider>
+          <Content>
+            <Box pos={'absolute'} top={'30px'} right={'30px'} zIndex={100} >
+              <MapSelector />
+            </Box>
+            {mode === '词云' && (
+              <Container
+                items={province}
+                type={"text"}
+                radius={20}
+                width={"50vw"}
+                height={"100%"}
+                color={colorMode === "light" ? "#666666" : "#fff"}
+                fontSize={1.2}
+                font={font}
+                clickEvent={(e, item) => {
+                  console.log(item);
+                  setArea(item);
+                }}
+              // style={{ zIndex: -1 }}
+              />
+            )}
+            {mode === '中国地图' && (
+              <EchartsMap data={[]} max={0} style={{ width: '50vw', height: "100%", paddingTop: '100px' }} clickEvent={(params) => setArea(params.name + '省')} />
+            )}
+          </Content>
+          <Sider
+            position="right"
+            style={{
+              height: "100%",
+              width: "25vw",
+              borderLeft: `3px solid ${colorMode === "light" ? "#f9f9f9" : "#1b2d50"
+                }`,
+              padding: "8px 16px",
             }}
-          // style={{ zIndex: -1 }}
-          />
+          >
+            <Card
+              style={{ width: "100%", height: "50vh", flex: 1 }}
+              position="right"
+              key={area + 3}
+            >
+              <CountryCharts option={{}} source={areaInfo[0]} />
+            </Card>
+            <Card
+              delay={300}
+              position="right"
+              key={area + 4}
+              style={{ width: "100%", height: "50vh", flex: 1 }}
+            >
+              <CountryCharts option={{}} />
+            </Card>
+          </Sider>
         </Content>
-        <Sider
-          position="right"
-          style={{
-            height: "100%",
-            width: "25vw",
-            borderLeft: `3px solid ${colorMode === "light" ? "#f9f9f9" : "#1b2d50"
-              }`,
-            padding: "8px 16px",
-          }}
-        >
-          <Card
-            style={{ width: "100%", height: "50vh", flex: 1 }}
-            position="right"
-            key={area + 3}
-          >
-            <CountryCharts option={{}} source={areaInfo[0]} />
-          </Card>
-          <Card
-            delay={300}
-            position="right"
-            key={area + 4}
-            style={{ width: "100%", height: "50vh", flex: 1 }}
-          >
-            <CountryCharts option={{}} />
-          </Card>
-        </Sider>
-      </Content>
+      </ModeContext.Provider>
     </LangContext.Provider>
   );
 }
