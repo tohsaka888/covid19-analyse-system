b07c220c8a87bda362ee70119c829c8e22c3f2b4
tohsaka888 <156132264@qq.com>
2022-06-01 17:22:47 +0800

    feat: 更新地图

--- src/components/EchartsMap/EchartsMap.tsx
+++ src/components/EchartsMap/EchartsMap.tsx
@@ -2,10 +2,14 @@ import React, { HTMLAttributes, useCallback, useEffect, useRef } from "react";
 import * as echarts from "echarts";
 import { chinaMapConfig } from "./mapConfig";
 import { ConfigProps } from "./type";
+import { AreaInfoResults } from "../../type";
 
-type Props = ConfigProps & HTMLAttributes<HTMLDivElement>;
+type Props = ConfigProps & HTMLAttributes<HTMLDivElement> & {
+  clickEvent: (params: any) => void;
+  info?: AreaInfoResults
+};
 
-function EchartsMap({ data, max, min, color, theme, ...props }: Props) {
+function EchartsMap({ data, max, min, color, theme, clickEvent, info, ...props }: Props) {
   // const [mapData, setMapData] = React.useState<any>([]);
   const mapRef = useRef<HTMLDivElement>(null!);
   const mapInstance = useRef<echarts.EChartsType>(null!);
@@ -18,30 +22,43 @@ function EchartsMap({ data, max, min, color, theme, ...props }: Props) {
       mapInstance.current = echarts.init(mapRef.current);
     }
     mapInstance.current.setOption(
-      chinaMapConfig({ data: [], max: 100, min: 0 })
+      chinaMapConfig({ data: [], max: 100, min: 0, theme: theme })
     );
-  }, []);
+    mapInstance.current.on("click", (params: any) => {
+      clickEvent(params)
+    });
+    return () => {
+      // 销毁实例，销毁后实例无法再被使用。
+      mapInstance && mapInstance.current.dispose();
+    };
+  }, [clickEvent, theme]);
 
   useEffect(() => {
+
+    let params = ''
+    if (!info) {
+      params = 'china'
+    } else {
+      params = `${info.locationId}`
+    }
     const getMapData = async () => {
-      const res = await fetch("https://geojson.cn/api/data/china.json");
+      const res = await fetch(`https://geojson.cn/api/data/${params}.json`);
       const data = await res.json();
-      // setMapData(data.features);
-      console.log(data);
       if (data) {
-        echarts.registerMap("china", { geoJSON: data });
+        console.log(data)
+        echarts.registerMap("China", { geoJSON: data });
         renderChart();
       }
     };
     getMapData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+
+  }, [info, renderChart]);
 
   // useEffect(() => {
   //   renderChart();
   // }, [renderChart]);
 
-  return <div ref={mapRef} {...props} />    ;
+  return <div ref={mapRef} {...props} />;
 }
 
 export default EchartsMap;
